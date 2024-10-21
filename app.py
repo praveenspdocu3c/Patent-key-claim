f2826113ceb5cc9c1b0b2397718f0c5c16d2a750
praveenspdocu3c <praveens@docu3c.com>
2024-10-21 23:53:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,19 +5,13 @@ from azure.core.credentials import AzureKeyCredential
 from docx import Document
 import os
 
-html_string='''
-<script>
-// To break out of iframe and access the parent window
-const streamlitDoc = window.parent.document;
-
-// Make the replacement
-document.addEventListener("DOMContentLoaded", function(event){
-        streamlitDoc.getElementsByTagName("footer")[0].innerHTML = "Provided by <a href='https://yourwebsite.com' target='_blank' class='css-z3au9t egzxvld2'>Your Link Display Text Here</a>";
-    });
-</script>
-'''
-components.html(html_string)
-
+hide_streamlit_style = """
+            <style>
+            [data-testid="stToolbar"] {visibility: hidden !important;}
+            footer {visibility: hidden !important;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 # Azure OpenAI setup
 client = AzureOpenAI(
