8659c0f6e43adea2a3a503c302e6705606cb7860
praveenspdocu3c <praveens@docu3c.com>
2024-10-21 23:50:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,13 +21,27 @@ import os
 # 	top: 2px;
 # }
 
-hide_streamlit_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+
+footer="
+
+<style> your css code put here</style>
+
+<div class='footer'>
+
+<p>the word you want to tell<a style='display:block;text-align:center;' 
+
+href='https://www.streamlit.io' target='_blank'>your email address put here</a></p>
+
+</div>"
+
+st.markdown(footer, unsafe_allow_html=True)
+# hide_streamlit_style = """
+#             <style>
+#             #MainMenu {visibility: hidden;}
+#             footer {visibility: hidden;}
+#             </style>
+#             """
+# st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
 # Azure OpenAI setup
 client = AzureOpenAI(
